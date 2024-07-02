b071ca172fc76c2f8eb702d027c73e26238badc7
sehbanfarooq <95231893+sehbanfarooq@users.noreply.github.com>
2024-07-02 22:13:35 +0530

    Delete myapp8 directory

--- myapp8/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-from myapp8.models import project
-class project(forms.ModelForm):
-    class Meta:
-        model=project
-        fields='_all_'
--- myapp8/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class project(models.Model):
-    pid=models.CharField(max_length=3,primary_key=True)
-    name=models.CharField(max_length=20)
-    language=models.CharField(max_length=30)
-    dutration=models.IntegerField(null=True)
